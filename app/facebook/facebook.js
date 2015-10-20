a21b25fbbdc9fd095088088b06343bf32130968b
Zuri Hunter <zurihunter@client-167.humangeo.local>
2015-10-20 12:00:24 -0400

    First COMMIT

---
+++ app/facebook/facebook.js
@@ -0,0 +1,73 @@
+'use strict';
+
+angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
+
+//SET UP ROUTE FOR THE NGSOCIAL FACEBOOK HOME PAGE
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/facebook', {
+    templateUrl: 'facebook/facebook.html',
+    controller: 'FacebookCtrl'
+  });
+}])
+
+//ADDS FACEBOOK APP ID AND THE LIST OF PERMISSIONS FOR THE APPLICATION
+.config( function ($facebookProvider){
+	$facebookProvider.setAppId('330433383798478');
+	$facebookProvider.setPermissions("email","public_profile", "user_posts", "publish_actions", "user_photos");
+})
+
+//FACEBOOK SDK FOR THE FACEBOOK API
+.run(function($rootScope){
+
+	 (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+})
+
+//FACEBOOK CONTROLLER
+.controller('FacebookCtrl', ['$scope', '$facebook', function($scope, $facebook) {
+  
+  
+  $scope.isLoggedIn = false;
+
+  //log in function
+  $scope.login = function(){
+    $facebook.login().then(function(){
+      $scope.isLoggedIn = true; 
+      refresh();
+    });
+  }
+
+  //log out function
+  $scope.logout = function(){
+    $facebook.logout().then(function(){
+      $scope.isLoggedIn = false; 
+      refresh();
+    });
+  }
+
+  //refresh feature so that it will show the updated information and user's content
+  function refresh(){
+    $facebook.api("/me").then(function(response){
+      $scope.welcomeMsg = "Welcome " + response.name; 
+      $scope.isLoggedIn = true; 
+      $scope.userInfo = response; 
+      $facebook.api('/me/picture').then(function(response){
+        $scope.picture = response.data.url; 
+      })
+    },
+
+    //error function, so if the user is not logged in this message appears
+    function(err){
+      $scope.welcomeMsg = "Please Log In"; 
+    });
+
+  }
+
+  refresh();
+}]);
